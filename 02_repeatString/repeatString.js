05901b663ebab2cd02f740209ceb5605927ff790
trojanekga <trojanekga@gmail.com>
2024-11-12 19:18:29 -0600

    Complete first two JS Array exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    else{
+    let finalString = "";
+    for (let i = 0; i < num; i++){
+        finalString = finalString + string;
+    }
+    return finalString;
+}
 };
 
 // Do not edit below this line
