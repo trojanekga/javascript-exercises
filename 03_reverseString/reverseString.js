7e032ec5cbd35023426bd6745cea4c96b8e3ba4b
trojanekga <trojanekga@gmail.com>
2024-11-13 19:55:07 -0600

    Finish reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str1 = string;
+    let backString = "";
+    
+    let split_string = str1.split("");
+    let strLength = split_string.length;
+    for (let i = 0; i < strLength; i++){
+        backString = backString + split_string.pop();
+    }
+    return backString;
 };
 
 // Do not edit below this line
