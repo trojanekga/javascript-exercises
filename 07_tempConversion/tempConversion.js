455a8a6b96a4a5143376ded4e081e69d2df3d1c2
Garrett Trojanek <trojanekga@gmail.com>
2024-11-24 22:27:48 -0600

    Finish tempConvert

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farTemp) {
+  let celsius = 0;
+
+  celsius = (farTemp - 32) * (5/9);
+  let celRound = celsius.toFixed(1);
+  let result1 = parseFloat(celRound);
+  return result1;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+
+  let fahrenheit = 0;
+  fahrenheit = (celTemp * (9/5) + 32);
+  let fahrRound = fahrenheit.toFixed(1);
+  let result2 = parseFloat(fahrRound);
+  return result2;
+
 };
 
 // Do not edit below this line
