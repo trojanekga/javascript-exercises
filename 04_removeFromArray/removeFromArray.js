ea652c50ea75479b5d4798a7bf06b3735991623f
Garrett Trojanek <trojanekga@gmail.com>
2024-11-24 19:36:15 -0600

    Finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, args) {
+    args = args;
+    arr = arr;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == args){
+            arr = arr.slice(i);
+        }
+        
+    }
+    return arr;
 };
 
 // Do not edit below this line
