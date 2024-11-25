9f6f7938846c7f0d7ce5a1c9263717a8701ddee5
Garrett Trojanek <trojanekga@gmail.com>
2024-11-24 22:13:29 -0600

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let finalSum = 0;
+    let c = 0;
+
+    if (a > b){
+        c = b;
+        b = a;
+        a = c;
+    }
+
+    if (a < 0 || b < 0){
+        return('ERROR');
+    }
+    if (Number.isInteger(a) && Number.isInteger(b)){
+
+    for (let i = a; i <= b; i++){
+        finalSum = finalSum + i;
+    }
+
+    return finalSum;
+}
+else {
+    return('ERROR');
+}
 
 };
 
